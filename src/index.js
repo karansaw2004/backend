f26aceb5f0d8a10089133fa5f5e6d4a4dd5b27e9
saw karan vinod <bs7819679@gmail.com>
2025-03-12 13:07:47 +0530

    add cookie-parser and cors middleware to enhance request handling

--- src/index.js
+++ src/index.js
@@ -4,12 +4,22 @@ import dotenv from 'dotenv';
 
 
 import connectDB from "./db/db.js";
+import { app } from './app.js';
 
 
 dotenv.config({path:'./env'})
 
 
-connectDB();
+connectDB()
+.then(()=>{
+    
+    app.listen(process.env.PORT,()=>{
+        console.log(`server is started at port no: ${process.env.PORT}`);
+    });
+})
+.catch((err)=>{
+    console.log("database connection Failed: ",err);
+})
 
 
 
